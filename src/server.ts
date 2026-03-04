44bd6889a43c2f9ac989fb874e93971898e510f3
F3anny <iribagizafanny13@gmail.com>
2026-03-04 22:06:45 +0200

    packages

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
-
-import Fastify from "fastify";
-const app=Fastify({
-    logger:true
-});
-app.get("/", async (request, reply) => {
-  return { message: "Elite backend started 🚀" };
-});
-const start = async () => {
-  try {
-    await app.listen({ port: 3000 });
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-};
+
+import Fastify from "fastify";
+
+const app=Fastify({
+    logger:true
+});
+app.get("/", async (request, reply) => {
+  return { message: "Elite backend started 🚀" };
+});
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
 start();
